a13664c5dcf96371c7f3bc628e10bda9df4c0955
fredericvl <34839323+fredericvl@users.noreply.github.com>
2021-10-08 17:48:15 +0200

    Release 0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-agua-iot",
-    version="0.0.7",
+    version="0.0.8",
     author="Frederic Van Linthoudt",
     author_email="frederic.van.linthoudt@gmail.com",
     description="py-agua-iot provides controlling heating devices connected via the IOT Agua platform of Micronova",
@@ -19,7 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-        "PyJWT==1.7.1",
+        "PyJWT==2.1.0",
         "requests==2.25.1",
     ],
 )
